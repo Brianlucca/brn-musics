ca2f1a5c756e0e6f4cad8bc94c6c021daeabc5ba
Brianlucca <brianlucca2002@gmail.com>
2024-03-25 14:45:08 -0300

    feat: adding reggae songs on screen.

--- src/components/musics/musics.js
+++ src/components/musics/musics.js
@@ -1,5 +1,15 @@
 import ForFirstTime from "./music/ForTheFirstTime.mp3";
 import Redrum from "./music/Redrum.mp3";
+import EsperancaNaoMorreu from "./music/AesperancaNaoMorreu.mp3"
+// import AmanhaTalvez from "./music/AmanhaTalvez.mp3"
+// import CaminhoDoSol from "./music/CaminhoDoSol.mp3"
+import ArcaDeFuga from "./music/ArcaDeFuga.mp3"
+import Arvore from "./music/Árvore.mp3"
+import BarradoNoBaile from "./music/BarradoNoBaile.mp3"
+import Camelo from "./music/Camelo.mp3"
+import CampoDeBatalha from "./music/CampoDeBatalha.mp3"
+
+
 
 export const musicas = [
   {
@@ -19,5 +29,59 @@ export const musicas = [
     musica: Redrum,
     imagem:
       "https://neonmusic.online/wp-content/uploads/2024/01/864851040cd7-768x432.jpg",
+  },
+  {
+    nome: "A Esperança Não Morreu",
+    banda: "Edson Gomes",
+    genero: "Raggae",
+    duracao: "4:20",
+    musica: EsperancaNaoMorreu,
+    imagem:
+      "https://i.scdn.co/image/91a5ccb34a9f7759df25f4a839ec7a6bbf07c7ca",
+  },
+  {
+    nome: "Arca de Fuga",
+    banda: "Edson Gomes",
+    genero: "Raggae",
+    duracao: "4:54",
+    musica: ArcaDeFuga,
+    imagem:
+      "https://i.scdn.co/image/91a5ccb34a9f7759df25f4a839ec7a6bbf07c7ca",
+  },
+  {
+    nome: "Árvore",
+    banda: "Edson Gomes",
+    genero: "Raggae",
+    duracao: "4:37",
+    musica: Arvore,
+    imagem:
+      "https://i.scdn.co/image/91a5ccb34a9f7759df25f4a839ec7a6bbf07c7ca",
+  },
+  {
+    nome: "Barrados no Baile",
+    banda: "Edson Gomes",
+    genero: "Raggae",
+    duracao: "4:39",
+    musica: BarradoNoBaile,
+    imagem:
+      "https://i.scdn.co/image/91a5ccb34a9f7759df25f4a839ec7a6bbf07c7ca",
+  },
+  {
+    nome: "Cameló",
+    banda: "Edson Gomes",
+    genero: "Raggae",
+    duracao: "4:11",
+    musica: Camelo,
+    imagem:
+      "https://i.scdn.co/image/91a5ccb34a9f7759df25f4a839ec7a6bbf07c7ca",
+  },
+  {
+    nome: "Campo de Batalha",
+    banda: "Edson Gomes",
+    genero: "Raggae",
+    duracao: "5:23",
+    musica: CampoDeBatalha,
+    imagem:
+      "https://i.scdn.co/image/91a5ccb34a9f7759df25f4a839ec7a6bbf07c7ca",
   },
 ];
